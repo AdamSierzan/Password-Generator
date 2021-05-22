f57b5ac4f2fb9dfbeeb716041abf8ed008de72fc
Sierzko <adagioo1993@gmail.com>
2021-05-22 11:48:03 +0200

    finishing script

--- password_generator.py
+++ password_generator.py
@@ -10,15 +10,25 @@ special_chars_clean_str = special_chars_clean.join(special_chars)
 numbers = string.digits
 
 all_sings = special_chars_clean_str + capital_letters + lower_case_letters + numbers
-
+letters = lower_case_letters + capital_letters
 password = []
 
 
 def password_generator():
-    number_of_chars = int(input("Hi! This is a password generator that will create unique password for you, how many characters you need: ?"))
-    for i  in range(number_of_chars):
-        random_character = random.choice(all_sings)
-        password.append(random_character)
+    strenght_of_password = input('Hi! This is a password generator that will create unique password for you,'
+                                'for strong password type "S", for easy password type "E": ')
+    
+    if strenght_of_password[0].lower() == "e":
+        number_of_chars = int(input("How many characters you need? "))
+        for i  in range(number_of_chars):
+            random_character = random.choice(letters)
+            password.append(random_character)
+    if strenght_of_password[0].lower() == "s":
+        number_of_chars = int(input("How many characters you need? "))
+        for i  in range(number_of_chars):
+            random_character = random.choice(all_sings)
+            password.append(random_character)
+
 
 password_generator()
 
